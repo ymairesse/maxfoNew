59e3473ad927d694ea6232537dc8b1dd48f38ee0
ym <ymairesse@sio2.be>
2023-03-07 09:32:22 +0100

    Correction table users

--- sql/ox_users.sql
+++ sql/ox_users.sql
@@ -3,14 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : sam. 22 oct. 2022 à 18:37
--- Version du serveur : 10.6.7-MariaDB-2ubuntu1.1
--- Version de PHP : 8.1.2
+-- Généré le : mar. 07 mars 2023 à 09:29
+-- Version du serveur : 10.6.12-MariaDB-0ubuntu0.22.04.1
+-- Version de PHP : 8.1.2-1ubuntu2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `oxfam`
@@ -31,8 +36,16 @@ CREATE TABLE `ox_users` (
   `telephone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Téléphone',
   `adresse` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Adresse',
   `cpostal` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Code postal',
-  `commune` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Ville'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `commune` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Ville',
+  `homonyme` tinyint(4) NOT NULL DEFAULT 0 COMMENT 'Homonymie sur les prénoms'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `ox_users`
+--
+
+INSERT INTO `ox_users` (`acronyme`, `nom`, `prenom`, `statut`, `mail`, `telephone`, `adresse`, `cpostal`, `commune`, `homonyme`) VALUES
+('admadm', 'adm', 'adm', 'admin', 'nomail@sio2.be', '', '', '', '', 0);
 
 --
 -- Index pour les tables déchargées
